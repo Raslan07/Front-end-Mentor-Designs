bd4188369e25a906dec3d64e4f880153b940aa3d
Mostafa Raslan <raslanspeed007@gmail.com>
2025-09-13 07:10:56 +0300

    Grid System

---
+++ interactive-card-details-form-main/script.js
@@ -0,0 +1,23 @@
+
+
+// Front-card
+const creditNumber = document.querySelector("#credit-number");
+const userName = document.querySelector(".user");
+const Data = document.querySelector(".data");
+// Back-card
+const CVC = document.querySelector("#back-cvc");
+// Form section
+const name = document.querySelector("#name");
+const numberCredit = document.querySelector("#Number");
+const month =document.querySelector('#month');
+const year =document.querySelector('#year');
+const cvc =document.querySelector('#cvc');
+// Button filed
+const btn = document.querySelector('.btn');
+
+
+document.addEventListener("change" , ()=>{
+creditNumber.value = numberCredit.value;
+creditNumber.value = numberCredit.value;
+})
+
